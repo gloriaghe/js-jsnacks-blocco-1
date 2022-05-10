7a03962dfa3ac9dcaa21d7312df6cfe451a0ed8c
Gloria <gloghe@yahoo.it>
2022-05-10 15:46:20 +0200

    esercizio li e ul

---
+++ array ul e li/script.js
@@ -0,0 +1,20 @@
+const arrayUno = ["parola1-1", "parola2-1","parola3-1","parola4-1","parola5-1"];
+console.log(arrayUno)
+
+const arrayDue = ["parola1-2", "parola2-2"];
+
+const stampa = document.querySelector("ul");
+
+for (let i = 0; i < 5; i++) {
+    
+    const element = `<li> ${arrayUno[i]}<ul></ul></li>`;
+    stampa.innerHTML += element;
+
+    
+}
+
+for (let i = 0; i < 2; i++) {
+    const stampaDue = document.querySelector("li>ul");
+    const elementDue = `<li> ${arrayDue[i]}</li>`;
+    stampaDue.innerHTML += elementDue;
+    }
