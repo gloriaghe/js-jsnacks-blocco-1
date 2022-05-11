71c59f2b71a63876479e4da6adb836772d1deca9
Gloria <gloghe@yahoo.it>
2022-05-11 09:52:22 +0200

    ulteriore modifica

--- array ul e li/script.js
+++ array ul e li/script.js
@@ -1,6 +1,4 @@
 const arrayUno = ["parola1-1", "parola2-1","parola3-1","parola4-1","parola5-1"];
-console.log(arrayUno);
-
 const arrayDue = ["parola1-2", "parola2-2"];
 
 const stampa = document.querySelector("ul");
@@ -9,17 +7,16 @@ const stampa = document.querySelector("ul");
 for (let i = 0; i < arrayUno.length; i++) {
     
     const creatoli = document.createElement("li");
-    const newContent = document.createTextNode(arrayUno[i]);
+    const newContent = arrayUno[i];
     creatoli.append(newContent);
     const creatoul =document.createElement("ul");
     creatoli.append(creatoul);
 
     for (let i = 0; i < arrayDue.length; i++) {
         const creatoliDue = document.createElement("li");
-        const newContentDue = document.createTextNode(arrayDue[i]);
+        const newContentDue = arrayDue[i];
         creatoliDue.append(newContentDue);
         creatoul.append(creatoliDue);
-        
        }
 
     stampa.append(creatoli);
